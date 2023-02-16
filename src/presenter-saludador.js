a39c2288f7bb90391fd2cb1701abddf36c204985
Dayan <dayan.aguilar@ucb.edu.bo>
2023-02-16 09:42:15 -0400

    Añadimos inputs de gener y edad

--- src/presenter-saludador.js
+++ src/presenter-saludador.js
@@ -1,11 +1,14 @@
 import sumar from "./sumador";
 
 const nombre = document.querySelector("#primer-nombre");
+const edad = document.querySelector("#edad");
+const genero = document.querySelector("#genero");
+
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#saludador-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + "Hola " + nombre.value + "</p>";
+  div.innerHTML = "<p>" + "Hola " + nombre.value + edad.value + genero.value + "</p>";
 });
